c6454cfb96d4c60e945376aa5771044e374b951b
Peter Kovecses <kovecsespeter@gmail.com>
2022-11-18 11:25:01 +0100

    Add Singleton pattern implementation

---
+++ Creational/Singleton/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace Creational.Singleton
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> _lazy =
+            new(() => new Singleton());
+
+        public static Singleton Instance { get { return _lazy.Value; } }
+
+        private Singleton()
+        {
+        }
+    }
+}
