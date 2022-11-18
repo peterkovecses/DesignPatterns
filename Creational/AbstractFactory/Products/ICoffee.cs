a6b1fc0a9d7246861b25b35c2b1dc325e95f3255
Peter Kovecses <kovecsespeter@gmail.com>
2022-11-18 09:45:04 +0100

    Add project files.

---
+++ Creational/AbstractFactory/Products/ICoffee.cs
@@ -0,0 +1,25 @@
+﻿namespace Creational.AbstractFactory.Products
+{
+    public class ICoffee
+    {
+    }
+
+    public class NormalCoffee : ICoffee
+    {
+    }
+
+    public class AmericanCoffee : ICoffee
+    {
+    }
+
+    public class ItalianCoffee : ICoffee
+    {
+    }
+
+    public enum CoffeeStyle
+    {
+        Normal,
+        American,
+        Italian
+    }
+}
---
+++ Creational/FactoryMethod/Products/IPizza.cs
@@ -0,0 +1,47 @@
+﻿namespace Creational.FactoryMethod.Products
+{
+    public interface IPizza
+    {
+        IList<string> Toppings { get; }
+        DoughType Dough { get; }
+    }
+
+    public class NormalPizza : IPizza
+    {
+        public NormalPizza(IList<string> toppings)
+        {
+            Toppings = toppings;
+        }
+
+        public IList<string> Toppings { get; }
+        public DoughType Dough => DoughType.Normal;
+    }
+
+    public class NewYorkPizza : IPizza
+    {
+        public IList<string> Toppings { get; }
+        public DoughType Dough => DoughType.Thin;
+
+        public NewYorkPizza(IList<string> toppings)
+        {
+            Toppings = toppings;
+        }
+    }
+    public class ChicagoPizza : IPizza
+    {
+        public IList<string> Toppings { get; }
+        public DoughType Dough => DoughType.DeepDish;
+
+        public ChicagoPizza(IList<string> toppings)
+        {
+            Toppings = toppings;
+        }
+    }
+
+    public enum DoughType
+    {
+        Normal,
+        Thin,
+        DeepDish
+    }
+}
