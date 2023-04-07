f987f8ff5a18f7b01b6d97ebb9f0616506b02816
Peter Kovecses <kovecsespeter@gmail.com>
2023-04-07 11:04:34 +0200

    Simplify Factory Method

---
+++ Creational/FactoryMethod/PizzaStore.cs
@@ -0,0 +1,17 @@
+﻿namespace Creational.FactoryMethod
+{
+    public class Pizza
+    {
+        public PizzaDoughType DoughType { get; set; }
+
+        public static Pizza CreateChicagoStylePizza => new() { DoughType = PizzaDoughType.DeepDish };
+        public static Pizza CreateNewYorkStylePizza => new() { DoughType = PizzaDoughType.Thin };
+
+    }
+
+    public enum PizzaDoughType
+    {
+        Thin,
+        DeepDish
+    }
+}
