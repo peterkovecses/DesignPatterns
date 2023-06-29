62d8920f5c7798eaecc3f69ca3d77428eed88b90
Peter Kovecses <kovecsespeter@gmail.com>
2023-06-29 16:33:41 +0200

    Add client code to composite pattern example

--- Structural/Composite/ProductPackage.cs
+++ Structural/Composite/ProductPackage.cs
@@ -1,20 +1,32 @@
-﻿namespace Structural.Composite
+﻿namespace Structural.Composite;
+
+// Component
+public interface IProduct
 {
-    public interface IProduct
-    {
-        int Price { get; }
-    }
+    int Price { get; }
+}
 
-    public class Product : IProduct
-    {
-        public int Price { get; set; }
-    }
+// Leafe
+public class Product : IProduct
+{
+    public int Price { get; set; }
+}
 
-    public class ProductPackage : IProduct
-    {
-        public required IEnumerable<Product> Products { get; set; }
-        public int Price => Products.Sum(product => product.Price);
-    }
+// Composite
+public class ProductPackage : IProduct
+{
+    public required IEnumerable<Product> Products { get; set; }
+    public int Price => Products.Sum(product => product.Price);
+}
+
+// Client code
+public class ShoppingCart
+{
+    private readonly List<IProduct> _products = new();
+
+    public int Amount => _products.Sum(product => product.Price);
+
+    public void AddProduct(IProduct product) => _products.Add(product);
 }
 
 
